8ee1b55d9bec9f8f02169968b08c83d05f4b648d
RookieMo <frozenprince.k@gmail.com>
2014-04-23 16:26:48 +0700

    add JL face rm jump cause bug

--- src/AnimationLayer.js
+++ src/AnimationLayer.js
@@ -1,9 +1,9 @@
+
+
 var AnimationLayer = cc.Layer.extend({
     spriteSheet:null,
     runningAction:null,
     sprite:null,
-    jumpUpAction:null,
-    jumpDownAction:null,
 
     ctor:function () {
         this._super();
@@ -11,13 +11,12 @@ var AnimationLayer = cc.Layer.extend({
     },
     init:function () {
         this._super();
-
+        
         cc.SpriteFrameCache.getInstance().addSpriteFrames(s_runnerplist);
         this.spriteSheet = cc.SpriteBatchNode.create(s_runner);
         this.addChild(this.spriteSheet);
 
-        this.initAction();
-        /*
+        
         var animFrames = [];
         for (var i = 0; i < 8; i++) {
             var str = "runner" + i + ".png";
@@ -27,7 +26,7 @@ var AnimationLayer = cc.Layer.extend({
 
         var animation = cc.Animation.create(animFrames, 0.1);
         this.runningAction = cc.RepeatForever.create(cc.Animate.create(animation));
-        */
+        
 
         this.sprite = cc.Sprite.createWithSpriteFrameName("runner0.png");
         this.sprite.setPosition(cc.p(80, 160));
@@ -36,37 +35,4 @@ var AnimationLayer = cc.Layer.extend({
 
         this.scheduleUpdate();
     },
-    initAction:function () {
-        // init runningAction
-        var animFrames = [];
-        // num equal to spriteSheet
-        for (var i = 0; i < 8; i++) {
-            var str = "runner" + i + ".png";
-            var frame = cc.SpriteFrameCache.getInstance().getSpriteFrame(str);
-            animFrames.push(frame);
-        }
-        var animation = cc.Animation.create(animFrames, 0.1);
-        this.runningAction = cc.RepeatForever.create(cc.Animate.create(animation));
-        this.runningAction.retain();
-        // init jumpUpAction
-        animFrames = [];
-        for (var i = 0; i < 4; i++) {
-            var str = "runnerJumpUp" + i + ".png";
-            var frame = cc.SpriteFrameCache.getInstance().getSpriteFrame(str);
-            animFrames.push(frame);
-        }
-        animation = cc.Animation.create(animFrames, 0.2);
-        this.jumpUpAction = cc.Animate.create(animation);
-        this.jumpUpAction.retain();
-        // init jumpDownAction
-        animFrames = [];
-        for (var i = 0; i < 2; i++) {
-            var str = "runnerJumpDown" + i + ".png";
-            var frame = cc.SpriteFrameCache.getInstance().getSpriteFrame(str);
-            animFrames.push(frame);
-        }
-        animation = cc.Animation.create(animFrames, 0.3);
-        this.jumpDownAction = cc.Animate.create(animation);
-        this.jumpDownAction.retain();
-    }
 });
