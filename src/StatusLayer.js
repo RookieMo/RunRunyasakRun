3b8d35209fa564e5a1f9a9476009497f4d1ca464
RookieMo <frozenprince.k@gmail.com>
2014-05-13 21:13:55 +0700

    sent project

--- src/StatusLayer.js
+++ src/StatusLayer.js
@@ -12,7 +12,7 @@ var StatusLayer = cc.Layer.extend({
         var winSize = cc.Director.getInstance().getWinSize();
 
         this.labelMeter = cc.LabelTTF.create("Score      0 point", "Helvetica", 30);
-        this.labelMeter.setPosition(cc.p(winSize.width - 130, winSize.height - 30));
+        this.labelMeter.setPosition(cc.p(winSize.width - 140, winSize.height - 30));
         this.addChild(this.labelMeter);
     },
     updateMeter:function (px) {
